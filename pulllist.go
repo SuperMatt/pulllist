962f59b0ca576564511ba1e83c94751a42b17f11
Matthew Ames <git@supermatt.net>
2017-09-17 16:33:29 +0100

    Added function to show next/previous weeks, and added MORE to output.

--- pulllist.go
+++ pulllist.go
@@ -1,113 +1,131 @@
 package main
 
-import(
-    "net/http"
-    "fmt"
-    "time"
-    //"reflect"
-    "strconv"
-    "io/ioutil"
-    "encoding/json"
-    "sort"
-    "strings"
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+	//"reflect"
+	"encoding/json"
+	"io/ioutil"
+	"sort"
+	"strconv"
+	"strings"
+
+	"golang.org/x/crypto/ssh/terminal"
 )
 
-type AllJson struct {
-    Results map[string]Book
+type allJSON struct {
+	Results map[string]book
 }
 
-type Book struct {
-    Id string `json:"id"`
-    Fields BookFields
+type book struct {
+	ID     string `json:"id"`
+	Fields bookFields
 }
 
-type BookFields struct {
-    Title string `json:"dc_solr_sortable_title"`
-    Type string `json:"type"`
+type bookFields struct {
+	Title string `json:"dc_solr_sortable_title"`
+	Type  string `json:"type"`
 }
 
-func start_of_day(t time.Time) time.Time {
-    t_hour := t.Hour()
-    t_minute := t.Minute()
-    t_second := t.Second()
-    t_nanosecond := t.Nanosecond()
-    return t.Add(-time.Duration(t_hour)*time.Hour).Add(-time.Duration(t_minute)*time.Minute).Add(-time.Duration(t_second)*time.Second).Add(-time.Duration(t_nanosecond))
-}
+func midnight(t time.Time) (midnight time.Time) {
 
-func ToCamel(s string) string {
-	s = strings.ToLower(s)
-	ss := strings.Split(s, " ")
-	var cs []string
-	for _,v := range(ss) {
-		if len(v) > 0 {
-			l := string(v[0])
-			ns := strings.Replace(v, l, strings.ToUpper(l), 1)
-			cs = append(cs, ns)
-		}
-	}
-	rs := strings.Join(cs, " ")
-	return rs
+	yy := t.Year()
+	mm := t.Month()
+	dd := t.Day()
+
+	return time.Date(yy, mm, dd, 0, 0, 0, 0, time.UTC)
 }
 
 func main() {
-    now := time.Now()
-    day := now.Weekday()
+	advance := flag.Int("advance", 0, "How many weeks in advance to show")
+	jsonOnly := flag.Bool("json", false, "Show the JSON results only")
+	single := flag.Bool("1", false, "Single page mode")
+	flag.Parse()
 
-    days_until_wednesday := 0
-    if day < 3 {
-        days_until_wednesday = 3 - int(day)
-    } else if day > 3 {
-        days_until_wednesday = 10 - int(day)
-    }
+	date := time.Now()
+	date = date.Add(time.Duration(*advance*7*24) * time.Hour)
 
-    next_wednesday := now.Add(time.Duration(24*days_until_wednesday)*time.Hour)
-    next_thursday := now.Add(time.Duration(24*(days_until_wednesday + 1))*time.Hour)
+	day := date.Weekday()
 
-    next_wednesday_start := start_of_day(next_wednesday)
-    next_thursday_start := start_of_day(next_thursday)
+	daysUntilWednesday := 0
+	if day < 3 {
+		daysUntilWednesday = 3 - int(day)
+	} else if day > 3 {
+		daysUntilWednesday = 10 - int(day)
+	}
 
-    start := strconv.FormatInt(next_wednesday_start.Unix(), 10)
-    end := strconv.FormatInt(next_thursday_start.Unix(), 10)
+	nextWednesday := date.Add(time.Duration(24*daysUntilWednesday) * time.Hour)
+	nextThursday := date.Add(time.Duration(24*(daysUntilWednesday+1)) * time.Hour)
 
-    booktype := "comic%7Cgraphic_novel"
+	nextWednesdayStart := midnight(nextWednesday)
+	nextThursdayStart := midnight(nextThursday)
 
-    url := "http://www.dccomics.com/proxy/search?type=" + booktype + "&startdate=" + start + "&enddate=" + end
-    //fmt.Println(url)
+	start := strconv.FormatInt(nextWednesdayStart.Unix(), 10)
+	end := strconv.FormatInt(nextThursdayStart.Unix(), 10)
 
-    resp, err := http.Get(url)
-    if err != nil {
-        fmt.Println(err.Error())
-    }
+	booktype := "comic%7Cgraphic_novel"
 
-    body, _ := ioutil.ReadAll(resp.Body)
-    //fmt.Println(string(body))
+	url := "http://www.dccomics.com/proxy/search?type=" + booktype + "&startdate=" + start + "&enddate=" + end
+	//fmt.Println(url)
 
-    var alljson AllJson
-    err = json.Unmarshal(body, &alljson)
-    if err != nil {
-        fmt.Println(err.Error())
-    }
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 
-    var comics []string
-    var graphic_novels []string
+	body, _ := ioutil.ReadAll(resp.Body)
+	if *jsonOnly {
+		fmt.Println(string(body))
+		os.Exit(0)
+	}
 
-    for _, v := range alljson.Results {
-        if v.Fields.Type == "comic" {
-            comics = append(comics, ToCamel(v.Fields.Title))
-        } else if v.Fields.Type == "graphic_novel" {
-            graphic_novels = append(graphic_novels, ToCamel(v.Fields.Title))
-        }
-    }
+	var alljson allJSON
+	err = json.Unmarshal(body, &alljson)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 
-    sort.Strings(comics)
-    sort.Strings(graphic_novels)
-    fmt.Println("Comics:")
-    for _, v := range(comics) {
-        fmt.Println(v)
-    }
-	fmt.Println("")
-    fmt.Println("Graphic Novels:")
-    for _, v := range(graphic_novels) {
-        fmt.Println(v)
-    }
+	var comics []string
+	var graphicNovels []string
+
+	for _, v := range alljson.Results {
+		if v.Fields.Type == "comic" {
+			comics = append(comics, strings.Title(strings.ToLower(v.Fields.Title)))
+		} else if v.Fields.Type == "graphic_novel" {
+			graphicNovels = append(graphicNovels, strings.Title(strings.ToLower(v.Fields.Title)))
+		}
+	}
+
+	sort.Strings(comics)
+	sort.Strings(graphicNovels)
+
+	_, height, err := terminal.GetSize(0)
+	maxlines := height - 1
+
+	var list []string
+
+	list = append(list, fmt.Sprintf("Comics:\n"))
+	for _, v := range comics {
+		list = append(list, fmt.Sprintf("%v\n", v))
+	}
+	list = append(list, fmt.Sprintf("\n"))
+	list = append(list, fmt.Sprintf("Graphic Novels:\n"))
+	for _, v := range graphicNovels {
+		list = append(list, fmt.Sprintf("%v\n", v))
+	}
+
+	for k, v := range list {
+		if k == 0 || k%maxlines != 0 {
+			fmt.Printf(v)
+			continue
+		}
+		if !*single {
+			var input string
+			fmt.Fprintf(os.Stderr, "MORE")
+			fmt.Scanln(&input)
+		}
+	}
 }
